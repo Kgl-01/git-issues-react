ea7be1446e001c35ef815505b685ca2d5bcbb75a
Kgl-01 <karthikgowda.mailme@gmail.com>
2023-02-10 08:59:38 +0530

    added media query to styles for responsive design phone device

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.scss";
-import Comments from "./components/comment/comment.component";
 import Issue from "./pages/issue-page/issue.page";
 import IssuesListPage from "./pages/issues-list-page/issues-list.page";
 
